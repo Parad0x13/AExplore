3e97ba077be3c40d3cacf16ada96a8c9b13029a6
Bryan Tremblay <bryanandhallie@gmail.com>
2020-06-09 06:09:58 -0400

    Added gitignore and updated game loop

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ C = Canvas(top, bg = "black", width = 320, height = 200, borderwidth = 0, highli
 C.pack()
 
 scale = 5.0    # Global to canvas
-def renderFont(fontManager, index, x, y, color = "limegreen"):
+def renderFont(fontManager, index, x, y, color = "limegreen", tags = ""):
     x *= fontManager.fontW * scale
     y *= fontManager.fontH * scale
 
@@ -20,10 +20,30 @@ def renderFont(fontManager, index, x, y, color = "limegreen"):
                 y1 = y + (Y * scale)
                 x2 = x1 + scale
                 y2 = y1 + scale
-                C.create_rectangle(C.canvasx(x1), C.canvasy(y1), C.canvasx(x2), C.canvasy(y2), fill = color, width = 0)
+                C.create_rectangle(C.canvasx(x1), C.canvasy(y1), C.canvasx(x2), C.canvasy(y2), fill = color, width = 0, tags = tags)
 
 fm = FontManager("BM437.png", 64, 4, 8, 16)
-renderFont(fm, 1, 0, 0, "blue")
-renderFont(fm, 2, 1, 1, "yellow")
 
+renderFont(fm, 1, 0, 0, "blue", tags = "player")
+renderFont(fm, 2, 1, 1, "red", tags = "enemy")
+
+def onKeyPressed(e):
+    key = e.keysym
+
+    deltaX = 8 * scale
+    deltaY = 16 * scale
+
+    if key == "w": C.move("player", 0, -deltaY)
+    if key == "s": C.move("player", 0, deltaY)
+    if key == "a": C.move("player", -deltaX, 0)
+    if key == "d": C.move("player", deltaX, 0)
+
+    C.update()
+
+C.bind_all("<Key>", onKeyPressed)
+
+def tick():
+    top.after(16, tick)
+
+top.after(16, tick)
 top.mainloop()
